3684ada12e8295393a06df5ecefbf8b35db14309
Bruno Bernardo <bigsby@live.com>
2017-04-19 15:52:54 +0100

    Add Hello World

---
+++ src/cs/hello.cs
@@ -0,0 +1,9 @@
+using System;
+
+static class Program
+{
+    public static void Main()
+    {
+        Console.Write("Hello, World!");
+    }
+}
---
+++ src/vb/hello.vb
@@ -0,0 +1,5 @@
+Public Module Program
+    Sub Main()
+        System.Console.Write("Hello, World!")
+    End Sub
+End Module
